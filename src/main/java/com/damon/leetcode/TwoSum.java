3e49e9f394296c4b91f5a0f2e85511f503946652
李洋 <liyang19@corp.netease.com>
2022-01-28 14:45:27 +0800

    2 sum

---
+++ src/main/java/com/damon/leetcode/TwoSum.java
@@ -0,0 +1,24 @@
+package com.damon.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    //one-pass hashtable
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> indexMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = target - nums[i];
+
+            if (indexMap.containsKey(num)) {
+                return new int[]{i, indexMap.get(num)};
+            } else {
+                indexMap.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
